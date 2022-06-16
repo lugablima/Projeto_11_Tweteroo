0a8c452192979f9210a6ff079c11a0caeb411024
LucasLima <lugablima@gmail.com>
2022-06-16 11:55:15 -0300

    chore: add persistence of user and tweet data

--- tweteroo-back-end/app.js
+++ tweteroo-back-end/app.js
@@ -5,4 +5,17 @@ const app = express();
 
 app.use(cors());
 
+const usersInfos = [];
+const tweetInfos = [];
+
+const userInfoTemplate = {
+  username: "",
+  avatar: "",
+};
+
+const tweetInfoTemplate = {
+  username: "",
+  tweet: "",
+};
+
 app.listen(5000);
