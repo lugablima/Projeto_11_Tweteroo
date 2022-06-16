366b47cfab21496ba7f5db6ae9f783e3c93eaf08
LucasLima <lugablima@gmail.com>
2022-06-16 13:40:23 -0300

    chore: add sign-up route

--- tweteroo-back-end/app.js
+++ tweteroo-back-end/app.js
@@ -4,14 +4,20 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 const usersInfos = [];
 const tweetInfos = [];
 
-const userInfoTemplate = {
-  username: "",
-  avatar: "",
-};
+app.post("/sign-up", (req, res) => {
+  usersInfos.push(req.body);
+  res.send("OK");
+});
 
 const tweetInfoTemplate = {
   username: "",
